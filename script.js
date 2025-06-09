c551a8ae076bd413ff894c76c9b8455405c2d1af
Lul1zarraga <lucianaalizarragaa@gmail.com>
2025-06-09 03:12:03 -0300

    video

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
     textoAnimado.style.display = "none";
     textoFijo.style.display = "block";
   });
+
+  const video = document.getElementById('videoIntro');
+  const contenido = document.getElementById('contenido');
+
+  video.addEventListener('ended', () => {
+    video.style.display = 'none';  
+    contenido.style.display = 'block'; 
+  });
 });
