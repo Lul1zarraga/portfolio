6cfcdff5f1a4d006315e42213de1773f9fa5c24a
Lul1zarraga <lucianaalizarragaa@gmail.com>
2025-06-08 22:27:37 -0300

    java

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
 document.addEventListener("DOMContentLoaded", function () {
+
   const textoAnimado = document.getElementById("texto-animado");
   const textoFijo = document.getElementById("texto-fijo");
+
 
   textoAnimado.addEventListener("animationend", function () {
     textoAnimado.style.display = "none";
     textoFijo.style.display = "block";
   });
-}); 
+});
