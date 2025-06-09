7e3ab40136d8e5503cc9e5568bb188ce7b26f889
Lul1zarraga <lucianaalizarragaa@gmail.com>
2025-06-08 21:07:13 -0300

    cambios

--- script.js
+++
@@ -1,7 +0,0 @@
-const animado = document.getElementById('texto-animado');
-const fijo = document.getElementById('texto-fijo');
-
-animado.addEventListener('animationend', () => {
-  animado.style.display = 'none'; 
-  fijo.style.display = 'block'; 
-}); 
